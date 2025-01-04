de7b734f0814530e45421aec75f2e6adbe326bb1
EzHavoc <kanishksingh778@gmail.com>
2025-01-04 14:28:41 +0530

    NavBar Changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,24 +8,43 @@ const NavBar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex items-center">
-      <img src={logo} alt="logo" className="mx-6 w-36 h-auto rounded-lg" />
+        <img src={logo} alt="logo" className="mx-6 w-36 h-auto rounded-lg" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-3xl">
-        <a href="https://www.linkedin.com/in/kanishk-singh-a097a2117/" target="_blank" rel="noopener noreferrer">
-          <FaLinkedin style={{ color: "white" }} />
+        <a
+          href="https://www.linkedin.com/in/kanishk-singh-a097a2117/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-blue-500 transition-colors"
+        >
+          <FaLinkedin />
         </a>
-        <a href="https://www.instagram.com/_kan.i.shk_/" target="_blank" rel="noopener noreferrer">
-          <FaInstagram style={{ color: "white" }} />
+        <a
+          href="https://www.instagram.com/_kan.i.shk_/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-pink-500 transition-colors"
+        >
+          <FaInstagram />
         </a>
-        <a href="https://github.com/EzHavoc" target="_blank" rel="noopener noreferrer">
-          <FaGithub style={{ color: "white" }} />
+        <a
+          href="https://github.com/EzHavoc"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-black transition-colors"
+        >
+          <FaGithub />
         </a>
-        <a href="https://x.com/KanishkS40568" target="_blank" rel="noopener noreferrer">
-          <FaSquareXTwitter style={{ color: "white" }} />
+        <a
+          href="https://x.com/KanishkS40568"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-500 transition-colors"
+        >
+          <FaSquareXTwitter />
         </a>
       </div>
     </nav>
   );
 };
-
 export default NavBar;
